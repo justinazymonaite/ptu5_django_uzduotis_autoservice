ab0ea88ae2844ec6adac829f373c00f68987e488
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-09 18:55:07 +0200

    django projekto vystymas 4 paskaita

--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Car, Order, Service, CarModel, OrderLine
 
 def index(request):
-    return HttpResponse("Welcome!")
+    # return HttpResponse("Welcome!")
+    car_count = Car.objects.count()
+    car_model_count = CarModel.objects.count()
+    order_line_count = OrderLine.objects.count()
+    order_count = Order.objects.count()
+
+    context = {
+        'car_count': car_count,
+        'car_model_count': car_model_count,
+        'order_line_count': order_line_count,
+        'order_count': order_count,
+        'service_count': Service.objects.count()
+    }
+    
+    return render(request, 'autoservice/index.html', context=context)
