939f8b3ff2fc8de58ed1780df4c0eef3885e7928
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-11 16:01:03 +0200

    django projekto vystymas 5 paskaitos uzduotys

--- ptu5_autoservice/autoservice/urls.py
+++ ptu5_autoservice/autoservice/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cars/', views.cars, name='cars'),
+    path('cars/<int:car_id>/', views.car_info, name='car_info'),
+    path('orders/', views.OrderListView.as_view(), name='orders'),
+    path('orders/<int:pk>/', views.OrderDetailView.as_view(), name='order'),
 ]
--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from . models import Car, Order, Service, CarModel, OrderLine
 
 def index(request):
@@ -18,3 +18,46 @@ def index(request):
     }
     
     return render(request, 'autoservice/index.html', context=context)
+
+def cars(request):
+    return render(request, 'autoservice/cars.html', {'cars': Car.objects.all()})
+
+def car_info(request, car_id):
+    return render(request, 'autoservice/car_info.html', {'car': get_object_or_404(Car, id=car_id)})
+
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'autoservice/order_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['orders_count'] = self.get_queryset().count()
+        return context
+
+
+class OrderDetailView(DetailView):
+    model = OrderLine
+    template_name = 'autoservice/order_detail.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        order_id = self.request.GET.get('order_id')
+        if order_id:
+            queryset = queryset.filter(order__id=order_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['books_count'] = Book.objects.count()
+        context['services_count'] = self.get_queryset().count()
+        order_id = self.request.GET.get('order_id')
+        context['orders'] = Order.objects.all()
+        if order_id:
+            context['genre'] = get_object_or_404(Order, id=order_id)
+        return context
