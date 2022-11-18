62b255d021db488d1b5dab2360272ff0b2175a7c
Justina Zymonaite <jzymonaite@yahoo.com>
2022-11-18 15:59:47 +0200

    django signalai

--- ptu5_autoservice/user_profile_1/apps.py
+++ ptu5_autoservice/user_profile_1/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UserProfile1Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_profile_1'
+
+    def ready(self):
+        from . signals import create_profile, save_profile
